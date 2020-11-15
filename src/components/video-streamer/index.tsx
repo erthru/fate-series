9ae76fbb5227f10e84e1dcd2a98630243ff71e5e
Suprianto Djmlu <erthru@MacBook-Pro.local>
2020-11-15 22:41:27 +0800

    remove use state

--- src/components/video-streamer/index.tsx
+++ src/components/video-streamer/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Store } from "../../plugins/store";
 import { continueVideo, pauseVideo, playVideo } from "../../plugins/store/video-streamer/actions";
