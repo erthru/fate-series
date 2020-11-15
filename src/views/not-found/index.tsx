3d378f8f3ef582dbec10e7c04382f2e8cc48b3f2
Suprianto Djmlu <erthru@MacBook-Pro.local>
2020-11-15 17:17:53 +0800

    add some title on views

--- src/views/not-found/index.tsx
+++ src/views/not-found/index.tsx
@@ -1,14 +1,22 @@
 import React from "react";
+import { Helmet } from "react-helmet";
+import { APP_NAME } from "../../helpers/contants";
 
 const NotFound = () => (
-    <div className="h-screen w-full pt-4 md:pt-16">
-        <div className="flex flex-wrap w-full">
-            <div className="flex w-full">
-                <span className="text-6xl text-white font-bold mx-auto">404</span>
-            </div>
+    <div>
+        <Helmet>
+            <title>Not Found | {APP_NAME}</title>
+        </Helmet>
 
-            <div className="flex w-full">
-                <span className="text-lg text-white font-medium mx-auto">Requested Page Not Found</span>
+        <div className="h-screen w-full pt-4 md:pt-16">
+            <div className="flex flex-wrap w-full">
+                <div className="flex w-full">
+                    <span className="text-6xl text-white font-bold mx-auto">404</span>
+                </div>
+
+                <div className="flex w-full">
+                    <span className="text-lg text-white font-medium mx-auto">Requested Page Not Found</span>
+                </div>
             </div>
         </div>
     </div>
--- src/views/show/index.tsx
+++ src/views/show/index.tsx
@@ -10,6 +10,8 @@ import { Store } from "../../plugins/store";
 import { getContent } from "../../plugins/store/content/actions";
 import { Content, ContentType } from "../../plugins/store/content/types";
 import { registerVideo } from "../../plugins/store/video-streamer/actions";
+import { Helmet } from "react-helmet";
+import { APP_NAME } from "../../helpers/contants";
 
 type Params = {
     id: string;
@@ -59,33 +61,39 @@ const Show = () => {
     }, [content]);
 
     return (
-        <div className="pt-10 flex flex-wrap">
-            {isFetchingContent && content.id === undefined ? <ProgressBar className="mx-auto" color="white" /> : null}
+        <div>
+            <Helmet>
+                <title>{content.title + " | " + APP_NAME}</title>
+            </Helmet>
 
-            <div className="w-full">
-                <Breadcrumb items={breadcrumbItems} />
-            </div>
+            <div className="pt-10 flex flex-wrap">
+                {isFetchingContent && content.id === undefined ? <ProgressBar className="mx-auto" color="white" /> : null}
 
-            <div className="w-full mt-10">
-                <MainVideoStreamer />
-            </div>
-
-            {content.type === ContentType.series ? (
-                <div>
-                    <div className="w-full mt-10">
-                        <MenuTitle title="Episodes" />
-                    </div>
-
-                    <div className="w-full mt-2 flex flex-wrap">{Episodes}</div>
+                <div className="w-full">
+                    <Breadcrumb items={breadcrumbItems} />
                 </div>
-            ) : null}
 
-            <div className="w-full mt-10">
-                <MenuTitle title="Comments" />
-            </div>
+                <div className="w-full mt-10">
+                    <MainVideoStreamer />
+                </div>
 
-            <div className="w-full mt-10">
-                <MenuTitle title="Add Comment" />
+                {content.type === ContentType.series ? (
+                    <div>
+                        <div className="w-full mt-10">
+                            <MenuTitle title="Episodes" />
+                        </div>
+
+                        <div className="w-full mt-2 flex flex-wrap">{Episodes}</div>
+                    </div>
+                ) : null}
+
+                <div className="w-full mt-10">
+                    <MenuTitle title="Comments" />
+                </div>
+
+                <div className="w-full mt-10">
+                    <MenuTitle title="Add Comment" />
+                </div>
             </div>
         </div>
     );
