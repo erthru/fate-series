3628296fb82175181bc20bd009dbb98b8a4c200f
Suprianto Djmlu <erthru@MacBook-Pro.local>
2020-11-15 19:24:47 +0800

    fix image fit

--- src/components/comment-item/index.tsx
+++ src/components/comment-item/index.tsx
@@ -9,10 +9,10 @@ type Props = {
 
 const CommentItem = (props: Props) => (
     <div className="flex w-full">
-        <img src={props.comment.avatar} alt="avatar" className="rounded-full w-12 h-12" />
+        <img src={props.comment.avatar} alt="avatar" className="rounded-full w-12 h-12" style={{ objectFit: "cover" }} />
 
         <div className="relative w-full md:w-8/12">
-            <Icon icon={faCaretLeft} className="text-indigo-1100 text-2xl absolute ml-4 mt-3"/>
+            <Icon icon={faCaretLeft} className="text-indigo-1100 text-2xl absolute ml-4 mt-3" />
 
             <div className="bg-indigo-1100 flex-1 p-4 ml-6 rounded-lg flex flex-wrap">
                 <span className="text-white w-full">{props.comment.name}</span>
